c24fcd692d169522279e981a0db6ea994744e9a2
leedujong <gkwkdlawoh@naver.com>
2018-04-03 17:36:47 +0900

    main 정리

--- app/src/main/java/com/hr/a1/MainActivity.java
+++ app/src/main/java/com/hr/a1/MainActivity.java
@@ -1,20 +1,12 @@
 package com.hr.a1;
 
-import android.graphics.drawable.Icon;
 import android.os.Build;
-import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
-import android.support.design.widget.AppBarLayout;
-import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v7.app.AppCompatActivity;
 
 
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -29,22 +21,10 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-
     ImageView textView;
     TextView textView2;
     TextView textView3;
     TextView textView4;
-
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    private SectionsPagerAdapter mSectionsPagerAdapter;
-
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
@@ -52,10 +32,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setMainLayout();
+    }
+
+    public void setMainLayout() {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
 
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
@@ -87,22 +68,33 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        wrapTabIndicatorToTitle(tabLayout, 10, 10);
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.home));
+        tabLayout.addTab(tabLayout.newTab().setText("TAB 2"));
+        tabLayout.addTab(tabLayout.newTab().setText("TAB 3"));
+        tabLayout.addTab(tabLayout.newTab().setText("TAB 4"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        textView = (ImageView) (((LinearLayout) ((LinearLayout) tabLayout.getChildAt(0)).getChildAt(0)).getChildAt(0));
+        textView2 = (TextView) (((LinearLayout) ((LinearLayout) tabLayout.getChildAt(0)).getChildAt(1)).getChildAt(1));
+        textView3 = (TextView) (((LinearLayout) ((LinearLayout) tabLayout.getChildAt(0)).getChildAt(2)).getChildAt(1));
+        textView4 = (TextView) (((LinearLayout) ((LinearLayout) tabLayout.getChildAt(0)).getChildAt(3)).getChildAt(1));
+        textView.setScaleY(-1);
+        textView2.setScaleY(-1);
+        textView3.setScaleY(-1);
+        textView4.setScaleY(-1);
+
+        tabLayout.requestLayout();
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
@@ -113,22 +105,14 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
     public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
         private static final String ARG_SECTION_NUMBER = "section_number";
 
         public PlaceholderFragment() {
         }
 
         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment1, container, false);
 
             TextView textView = (TextView) rootView.findViewById(R.id.section_label);
@@ -136,65 +120,5 @@ public class MainActivity extends AppCompatActivity {
 
             return rootView;
         }
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentStatePagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            Fragment fragment = null;
-            switch (position) {
-                case 0:
-                    fragment = new Fragment1();
-                    break;
-                case 1:
-                    fragment = new Fragment2();
-                    break;
-                case 2:
-                    fragment = new Fragment3();
-                    break;
-                case 3:
-                    fragment = new Fragment4();
-                    break;
-            }
-            return fragment;
-        }
-
-        @Override
-        public int getCount() {
-            // Show 4 total pages.
-            return 4;
-        }
-    }
-
-    public void wrapTabIndicatorToTitle(TabLayout tabLayout, int externalMargin, int internalMargin) {
-        View tabStrip = tabLayout.getChildAt(0);
-
-        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.home));
-        tabLayout.addTab(tabLayout.newTab().setText("TAB 2"));
-        tabLayout.addTab(tabLayout.newTab().setText("TAB 3"));
-        tabLayout.addTab(tabLayout.newTab().setText("TAB 4"));
-        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
-
-//        textView = (ImageView) (((LinearLayout) ((LinearLayout) tabLayout.getChildAt(0)).getChildAt(0)).getChildAt(1));
-        textView2 = (TextView) (((LinearLayout) ((LinearLayout) tabLayout.getChildAt(0)).getChildAt(1)).getChildAt(1));
-        textView3 = (TextView) (((LinearLayout) ((LinearLayout) tabLayout.getChildAt(0)).getChildAt(2)).getChildAt(1));
-        textView4 = (TextView) (((LinearLayout) ((LinearLayout) tabLayout.getChildAt(0)).getChildAt(3)).getChildAt(1));
-//        textView.setScaleY(-1);
-        textView2.setScaleY(-1);
-        textView3.setScaleY(-1);
-        textView4.setScaleY(-1);
-
-        tabLayout.requestLayout();
     }
 }
